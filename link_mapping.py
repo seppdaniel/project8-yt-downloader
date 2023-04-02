b0aef456d9af74650d926607fa40d79de91b417e
Daniel Silva <daniel.silva@madeiramadeira.com.br>
2023-04-02 06:52:23 -0300

    commit

---
+++ link_mapping.py
@@ -0,0 +1,7 @@
+
+video_links = [
+    'veqQXYhJNeg', # link 1 - Tesseract - Notcturne (P O R T A L S)
+    'e5PveLOphtU', # link 2 - Tesseract - Tourniquet (P O R T A L S)
+    '8NcxUzH9KrM', # link 3 - Tesseract - Tourniquet (P O R T A L S)
+    # ... adicione mais links conforme necessário
+]
